f61b02e3cf518c31cd463219e8dd27e8e0821f06
Milla guerrero E <78340596+guerrerosystem@users.noreply.github.com>
2021-07-18 23:51:19 -0500

    codigo

---
+++ app/src/main/java/com/example/loginconnumtelefono/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.loginconnumtelefono;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mLogOutBtn;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLogOutBtn = findViewById(R.id.log_out_btn);
+        mAuth = FirebaseAuth.getInstance();
+
+        mLogOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(new Intent(MainActivity.this , LoginActivity.class));
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null){
+            startActivity(new Intent(MainActivity.this , LoginActivity.class));
+            finish();
+        }
+    }
+}
